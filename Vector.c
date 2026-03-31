faced2a672fa7e96ff089e3825714ba0d4e3e9c6
buitronmariasol-cpu24 <maria.buitron.portilla@udla.edu.ec>
2026-03-31 12:11:43 -0500

    Add files via upload

---
+++ Vector.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main ()
+{
+
+   int n, i, A[50];
+
+   printf("Ingrese el numero de elementos al vestor A")
+   scanf("%d", &n);
+
+   for(i=0; i < n; i++)
+   {
+
+    printf("Ingrese A[%d]:", i);
+
+   }
+   return 0;
+}
